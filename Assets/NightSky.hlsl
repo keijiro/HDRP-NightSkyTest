3f9f0cd83e1f3ce8e32aa9fdbf06a2b905d91bf3
Keijiro Takahashi <keijiro@gmail.com>
2024-04-09 11:51:00 +0900

    Use ShaderGraph instead of hlsl

--- Assets/NightSky.hlsl
+++
@@ -1,7 +0,0 @@
-#include "Packages/jp.keijiro.noiseshader/Shader/SimplexNoise3D.hlsl"
-
-void Stars_float(float3 ViewDir, float Freq, float Power, float Amplify, out float Out)
-{
-    float n = SimplexNoise(ViewDir * Freq);
-    Out = pow(saturate(n), Power) * Amplify;
-}
